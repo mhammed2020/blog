92b9d5e326ae6605d2ca62714b575ea7a9ed03b0
jeddou mhammed <jeddou.ehtp@gmail.com>
2020-07-26 11:09:22 +0100

    Customizing the way that models are

--- blog/admin.py
+++ blog/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
-admin.site.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ['status','created']
+    list_display = ['title','created','status']
+    search_fields = ['title','body']
+admin.site.register(Post,PostAdmin)
+
